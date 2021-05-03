ef5def6f9be6d1bf893ffb4dd3ff12b0d52594a0
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-03 20:52:22 +0200

    Initial commit

---
+++ project2/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  // put setup code here
+
+  createCanvas(480, 120);
+}
+
+function draw() {
+  // put drawing code here
+
+   background(204);
+   point(240, 60);
+   point (1,119);
+   point (1,1);
+   point (479,1);
+   point (479,119);
+}
+
+   //if(mouseIsPressed) {
+   // fill(0);
+   //   } else {
+   //     fill(255);
+   //   }
+   //   ellipse(mouseX, mouseY, 80, 80);
+  //}

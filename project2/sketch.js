8c08427a017f7115867d95b29353655098056f69
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-03 22:02:40 +0200

    Drawing horizonal, vertical, and diagnoal lines

--- project2/sketch.js
+++ project2/sketch.js
@@ -10,15 +10,44 @@ function draw() {
    background(204);
    point(240, 60);
    point (1,119);
-   point (1,1);
+  
+ 
    point (479,1);
    point (479,119);
+  
+  //vertical
+  point (1,1);
+  point (1,2);
+  point (1,3);
+  point (1,4);
+  point (1,5);
+  point (1,6);
+  point (1,7);
+  point (1,8);
+  point (1,9);
+  point (1,10);
+  
+  //diagnoal
+  point (2,2);
+  point (2,3);
+  point (3,4);
+  point (4,5);
+  point (5,7);
+  point (7,8);
+  point (9,10);
+  point (11,12);
+  point (13,14);
+  point (14,15);
+  
+  //horizontal
+  point (2,1);
+  point (3,1);
+  point (4,1);
+  point (5,1);
+  point (6,1);
+  point (7,1);
+  point (8,1);
+  point (9,1);
+  point (10,1);
+  point (11,1);
 }
-
-   //if(mouseIsPressed) {
-   // fill(0);
-   //   } else {
-   //     fill(255);
-   //   }
-   //   ellipse(mouseX, mouseY, 80, 80);
-  //}

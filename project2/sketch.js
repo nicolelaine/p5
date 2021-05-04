52f96f24b7b6d79d8a56e90ba0420c8dd6104fea
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-04 17:43:20 +0200

    Draw a line

--- project2/sketch.js
+++ project2/sketch.js
@@ -6,48 +6,7 @@ function setup() {
 
 function draw() {
   // put drawing code here
-
-   background(204);
-   point(240, 60);
-   point (1,119);
+  background(204);
+  line(20,50, 420, 110);
   
- 
-   point (479,1);
-   point (479,119);
-  
-  //vertical
-  point (1,1);
-  point (1,2);
-  point (1,3);
-  point (1,4);
-  point (1,5);
-  point (1,6);
-  point (1,7);
-  point (1,8);
-  point (1,9);
-  point (1,10);
-  
-  //diagnoal
-  point (2,2);
-  point (2,3);
-  point (3,4);
-  point (4,5);
-  point (5,7);
-  point (7,8);
-  point (9,10);
-  point (11,12);
-  point (13,14);
-  point (14,15);
-  
-  //horizontal
-  point (2,1);
-  point (3,1);
-  point (4,1);
-  point (5,1);
-  point (6,1);
-  point (7,1);
-  point (8,1);
-  point (9,1);
-  point (10,1);
-  point (11,1);
 }

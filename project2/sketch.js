d233060da0cd618d22c49096042b1a29964346aa
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 18:23:42 +0200

    Drawing half ellipses using angleMode to change everything into degrees up front

--- project2/sketch.js
+++ project2/sketch.js
@@ -2,14 +2,15 @@ function setup() {
   // put setup code here
 
   createCanvas(480, 120);
+  angleMode(DEGREES);
 }
 
 function draw() {
   // put drawing code here
 
   background(204);
-  arc(90, 60, 80, 80, 0, radians(90));
-  arc(190, 60, 80, 80, 0, radians(270));
-  arc(290, 60, 80, 80, radians(180), radians(450));
-  arc(390, 60, 80, 80, radians(45), radians(225));
+  arc(90, 60, 80, 80, 0, 90);
+  arc(190, 60, 80, 80, 0, 270);
+  arc(290, 60, 80, 80, 180, 450);
+  arc(390, 60, 80, 80, 45, 225);
 }

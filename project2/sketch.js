77bcfd7064a1456c10fce1d11eeeb5334555c25b
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 18:34:18 +0200

    Drawing in order, cicle under rectangle

--- project2/sketch.js
+++ project2/sketch.js
@@ -2,15 +2,15 @@ function setup() {
   // put setup code here
 
   createCanvas(480, 120);
-  angleMode(DEGREES);
+
 }
 
 function draw() {
   // put drawing code here
 
   background(204);
-  arc(90, 60, 80, 80, 0, 90);
-  arc(190, 60, 80, 80, 0, 270);
-  arc(290, 60, 80, 80, 180, 450);
-  arc(390, 60, 80, 80, 45, 225);
+  ellipse(140, 0, 190, 190);
+  //the rectangle comes after the ellipse
+  //because it comes after it in the code
+  rect(160, 30, 260, 20);
 }

117e3cc126809a140614bf512732f2198714b077
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 18:02:17 +0200

    Drawing a simple rectangle

--- project2/sketch.js
+++ project2/sketch.js
@@ -7,8 +7,6 @@ function setup() {
 function draw() {
   // put drawing code here
   background(204);
-  quad (158, 55, 199, 14, 392, 66, 351, 107);
-  triangle (347, 54, 392, 9, 392, 66);
-  triangle (158, 55, 290, 91, 290, 112);
+  rect(180, 60, 220, 40);
   
 }

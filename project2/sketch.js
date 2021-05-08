e72f756438f4e72efc51c125ce83217f084642c6
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 21:40:09 +0200

    Playing with stoke attributes for rectangles and lines

--- project2/sketch.js
+++ project2/sketch.js
@@ -2,18 +2,20 @@ function setup() {
   // put setup code here
 
   createCanvas(480, 120);
+  strokeWeight(12);
 
 }
 
 function draw() {
   // put drawing code here
   background(204);
-  strokeWeight(1);
-  ellipse(75, 60, 90, 90);
-  strokeWeight(8); //stroke weight to 8 pixels
-  ellipse(175, 60, 90, 90);
-  ellipse(279, 60, 90, 90);
-  strokeWeight(20); //stroke weight to 20 pixels
-  ellipse(389, 60, 90, 90);
- 
+  strokeJoin(ROUND); //round the stoke corners
+  rect(40, 25, 70, 70);
+  strokeJoin(BEVEL) //bevel the stroke corners
+  rect(140, 25, 70, 70);
+  strokeCap(SQUARE) //square the line endings
+  line(270, 25, 340, 95);
+  strokeCap(ROUND) //round the line endings
+  line(350, 25, 420, 95);
+
 }

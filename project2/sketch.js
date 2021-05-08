91bfa6aebf0b67b59f817edcf56b14fa4c3829a7
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 20:47:19 +0200

    Playing around with strokeWeight on circles

--- project2/sketch.js
+++ project2/sketch.js
@@ -8,10 +8,12 @@ function setup() {
 function draw() {
   // put drawing code here
   background(204);
-  rect(160, 30, 260, 20);
-    //the ellipse comes after the rectangle
-  //because it comes after it in the code
-  ellipse(140, 0, 190, 190);
-
+  strokeWeight(1);
+  ellipse(75, 60, 90, 90);
+  strokeWeight(8); //stroke weight to 8 pixels
+  ellipse(175, 60, 90, 90);
+  ellipse(279, 60, 90, 90);
+  strokeWeight(20); //stroke weight to 20 pixels
+  ellipse(389, 60, 90, 90);
  
 }

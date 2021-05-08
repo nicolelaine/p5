1983de3af8acaa5721d55323cce570438be27f38
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-08 18:36:14 +0200

    Drawing order, putting the rectangle behind the cirlce

--- project2/sketch.js
+++ project2/sketch.js
@@ -7,10 +7,11 @@ function setup() {
 
 function draw() {
   // put drawing code here
-
   background(204);
-  ellipse(140, 0, 190, 190);
-  //the rectangle comes after the ellipse
-  //because it comes after it in the code
   rect(160, 30, 260, 20);
+    //the ellipse comes after the rectangle
+  //because it comes after it in the code
+  ellipse(140, 0, 190, 190);
+
+ 
 }

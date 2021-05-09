4856dab79fdd85371cebc9d9f2589a99da93ee76
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-09 10:33:29 +0200

    Controlling fill and stroke

--- project2/sketch.js
+++ project2/sketch.js
@@ -8,13 +8,14 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(0);                //Black
-  fill(204);                    //Light Gray
-  ellipse(132, 82, 200, 200);   //Light gray circle
-  fill(153);                    //Medium gray
-  ellipse(226, -16, 200, 200);  //Medium gray circle
-  fill(102);                    //Dark gray
-  ellipse(268, 118, 200, 200)    //dark gray circle
-
+  background(204);               //Light Gray
+  fill(153);                    //Medium Gray
+  ellipse(132, 82, 200, 200);   //Gray Circle
+  noFill();                    //Turn off fill
+  ellipse(226, -16, 200, 200);  //Outline circle with no fill
+  noStrokefill();                 //Turn off stroke
+  ellipse(268, 118, 200, 200)    //Doesn't draw!
+    //How come it doesn't just draw a  circle with no border
+    //around it? Hmmmmm....
 
 }

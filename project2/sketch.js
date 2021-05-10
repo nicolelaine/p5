3d6a296944181ac2076b57d9cd8ff53bb4fde732
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-10 22:21:12 +0200

    Playing with the transparency of RBG colors

--- project2/sketch.js
+++ project2/sketch.js
@@ -2,18 +2,16 @@ function setup() {
   // put setup code here
 
   createCanvas(480, 120);
-
-
+  noStroke();
 }
 
 function draw() {
   // put drawing code here
-  background(204);               //Light Gray
-  fill(153);                    //Medium Gray
-  ellipse(132, 82, 200, 200);   //Gray Circle
-  noFill();                    //Turn off fill
-  ellipse(226, -16, 200, 200);  //Outline circle with no fill
-  fill(255);
-  ellipse(268, 118, 200, 200);   //Doesn't draw!
- 
+  background(204, 226,225);               //Light Blue Color
+  fill(255, 0, 0, 160);                    //Red Color
+  ellipse(132, 82, 200, 200);         //Red Circle
+  fill(0, 255, 0, 160);                    //Green color
+  ellipse(226, -16, 200, 200);        //Green Circle
+  fill(0, 0, 255, 160);                    //Blue Color
+  ellipse(268, 118, 200, 200);        //Blue Circle
   }

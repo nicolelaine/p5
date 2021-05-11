bd8a47b006b469dad4f3b219a5fc3fd03b936ef4
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-11 18:46:36 +0200

    Creating P5 the robot for chapter 3

--- project2/sketch.js
+++ project2/sketch.js
@@ -1,45 +1,52 @@
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
+  createCanvas(720, 480);
+  strokeWeight(2);
+  ellipseMode(RADIUS);
 
 }
 
 function draw() {
   // put drawing code here
 
-  //left creature
+ 
   background(204); 
-  fill(255);
-  beginShape();
-  vertex(50, 120);     
-  vertex(100, 90);  
-  //vertex(110, 160);
-  vertex(80, 20);
-  vertex(210, 60);
-  vertex(160, 80);
-  vertex(200, 90);
-  vertex(140, 100);
-  vertex(130,120);
-  endShape();
-  fill(0);
-  ellipse(155, 60, 8, 8);
 
+  //Neck
 
-  //right creature
-  fill(255)
-  beginShape();
-  vertex(370, 120);
-  vertex(360, 90);
-  vertex(290, 80);
-  vertex(340, 70);
-  vertex(280, 50);
-  vertex(420, 10);
-  vertex(390, 50);
-  vertex(410, 90);
-  vertex(460, 120);
-  endShape();
-  fill(0);
-  ellipse(345, 50, 10, 10);
+  stroke(102); //set stroke to gray
+  line(266, 257, 266, 162); //left
+  line(276, 257, 276, 162); //Middle
+  line(286, 257, 286, 162); //Right
+
+  //Antennae
+
+  line(276, 155, 246, 112); //small
+  line(276, 155, 306, 56); //tall
+  line(276, 155, 342, 170); //medium
+
+  //body
+
+  noStroke();    //disable stroke
+  fill(102)      //set fill to gray
+  ellipse(264, 377, 33, 33);  //Antigravity orb
+  fill(0);                //set fill to black
+  rect (219, 257, 90, 120);   //main body
+  fill(102);              //set fill to gray
+  rect(219, 271, 90, 6);   //gray stripe
+
+  //head
+
+  fill(0);  //set fill to black
+  ellipse(276, 155, 45, 45);    //head
+  fill(255);              //set fill to white
+  ellipse(288, 150, 14, 14);   //large eye
+  fill(0);          //set fill to black
+  ellipse(288, 150, 3, 3);   //pupil
+  fill(153);         //set fill to light gray
+  ellipse(263, 148, 5, 5);   //small eye 1
+  ellipse(296, 130, 4, 4);   //small eye 2
+  ellipse(305, 162, 3, 3);   //small eye 3
 
   }

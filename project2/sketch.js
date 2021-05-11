99cb9c00f1dcb32da8b5415315e7478448f4bfb6
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-11 18:26:27 +0200

    Creating creatures

--- project2/sketch.js
+++ project2/sketch.js
@@ -7,14 +7,39 @@ function setup() {
 
 function draw() {
   // put drawing code here
+
+  //left creature
   background(204); 
+  fill(255);
   beginShape();
-  vertex(180, 82);     
-  vertex(207, 36);  
-  vertex(214, 63);
-  vertex(407, 11);
-  vertex(412, 30);
-  vertex(219, 82);
-  vertex(226, 109);
-  endShape(CLOSE);
+  vertex(50, 120);     
+  vertex(100, 90);  
+  //vertex(110, 160);
+  vertex(80, 20);
+  vertex(210, 60);
+  vertex(160, 80);
+  vertex(200, 90);
+  vertex(140, 100);
+  vertex(130,120);
+  endShape();
+  fill(0);
+  ellipse(155, 60, 8, 8);
+
+
+  //right creature
+  fill(255)
+  beginShape();
+  vertex(370, 120);
+  vertex(360, 90);
+  vertex(290, 80);
+  vertex(340, 70);
+  vertex(280, 50);
+  vertex(420, 10);
+  vertex(390, 50);
+  vertex(410, 90);
+  vertex(460, 120);
+  endShape();
+  fill(0);
+  ellipse(345, 50, 10, 10);
+
   }

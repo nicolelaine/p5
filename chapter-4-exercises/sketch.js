18cd53d8549eb142e764a3be3ba93934f000bc2f
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-13 17:09:22 +0200

    Using variable to adjust our drawings to the height and width of the canvas, so we can make entire drawings bigger

--- chapter-4-exercises/sketch.js
+++ chapter-4-exercises/sketch.js
@@ -1,19 +1,22 @@
 
-var y = 100;  //y variable 
-var d = 130;  //diameter variable
+//using width and heigh, which are set above in the createCanvas
+//helps us to move items around the exact canvas size
+//even if the canvas ends up changing size
+//which saves us tons of work in the end!
 
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
+  createCanvas(500, 200);
 }
 
 function draw() {
   // put drawing code here
 
    background(204);
-   ellipse (75, y, d, d); //left circle  //d and d as diamter is also width and heigh of this circle
-   ellipse (175, y, d, d); //middle cirlce
-   ellipse (275, y, d, d); //right circle
+  
+  line (0, 0, width, height); //line from (0,0) to (480, 120)
+  line (width, 0, 0, height);  //line from (480, 0) to (0, 120)
+  ellipse (width/2, height/2, 60, 60);
 
 }

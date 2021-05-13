a7f6134ac59b2a063c2836ab9ec12463bd650723
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-13 16:31:47 +0200

    Using variable to resuse the same values

---
+++ chapter-4-exercises/sketch.js
@@ -0,0 +1,19 @@
+
+var y = 60;  //y variable 
+var d = 80;  //diameter variable
+
+function setup() {
+  // put setup code here
+
+  createCanvas(480, 120);
+}
+
+function draw() {
+  // put drawing code here
+
+   background(204);
+   ellipse (75, y, d, d); //left circle  //d and d as diamter is also width and heigh of this circle
+   ellipse (175, y, d, d); //middle cirlce
+   ellipse (275, y, d, d); //right circle
+
+}

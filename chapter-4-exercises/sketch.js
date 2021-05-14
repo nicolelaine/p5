140ed89d3943d0454ef1c297300fb29f874acbd2
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-14 14:10:14 +0200

    Create a set of lines using a for loop

--- chapter-4-exercises/sketch.js
+++ chapter-4-exercises/sketch.js
@@ -11,13 +11,17 @@ function draw() {
   // put drawing code here
 
    background(204);
-  
-  line (20, 40, 80, 80);
-  line (80, 40, 140, 80);
-  line (140, 40, 200, 80);
-  line (200, 40, 260, 80);
-  line (260, 40, 320, 80);
-  line (320, 40, 380, 80);
-  line (380, 40, 440, 80);
+  //code that is not in a for loop
+  //line (20, 40, 80, 80);
+  //line (80, 40, 140, 80);
+  //line (140, 40, 200, 80);
+  //line (200, 40, 260, 80);
+  //line (260, 40, 320, 80);
+  //line (320, 40, 380, 80);
+  //line (380, 40, 440, 80);
 
+  //code that is in a for loop
+   for (var i = 20; i < 400; i +=60) {  
+     line (i, 40, i + 60, 80);
+   }
 }

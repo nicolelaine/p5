5e9d4da0124c49be5457ae7517b09a6fd1258b1c
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-14 20:32:37 +0200

    P5 robot variable 4

--- chapter-4-exercises/sketch.js
+++ chapter-4-exercises/sketch.js
@@ -1,19 +1,56 @@
 
+var x = 60;            // x coordinate
+var y = 420;            // y coordinate
+var bodyHeight = 110;   //Body height
+var neckHeight = 140;   //Neck height
+var radius = 45;
+var ny = y - bodyHeight - neckHeight - radius;  //Neck Y
+
 
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
+  createCanvas(170, 480);
+  strokeWeight(2);
+  ellipseMode(RADIUS);
 }
 
 function draw() {
   // put drawing code here
 
-   background(0);
+   background(204);
 
-   for (var y = 32; y <= height; y +=8) {  
-    for (var x = 12; x <= width-20; x+=15) {
-     ellipse (x + y, y, 16 - y/10.0, 16 - y/ 10.0);
-    }
-  }
+   //Neck 
+
+  stroke(102);
+  line (x+2, y-bodyHeight, x+2, ny);
+  line (x+12, y-bodyHeight, x+12, ny);
+  line (x+22, y-bodyHeight, x+22, ny);
+
+  //Antennae
+  line (x+12, ny, x-18, ny-43);
+  line (x+12, ny, x+42, ny-99);
+  line (x+12, ny, x+78, ny+15);
+
+  //Body
+  noStroke();
+  fill (102);
+  ellipse (x, y-33, 33, 33);
+  fill (0);
+  rect (x-45, y-bodyHeight, 90, bodyHeight-33);
+  fill (102);
+  rect (x-45, y-bodyHeight+17, 90, 6);
+
+  //Head
+  fill (0);
+  ellipse (x+12, ny, radius, radius);
+  fill (255);
+  ellipse (x+24, ny-6, 14, 14);
+  fill (0);
+  ellipse (x+24, ny-6, 3, 3);
+  fill (153);
+  ellipse (x, ny-8, 5, 5);
+  ellipse (x+30, ny-26, 4, 4);
+  ellipse (x+41, ny+6, 3, 3);
+  
 }

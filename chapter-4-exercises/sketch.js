3921e40ebf18ebbdc480515d83a6d1efd8827d80
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-14 19:53:25 +0200

    Creating rows and columns

--- chapter-4-exercises/sketch.js
+++ chapter-4-exercises/sketch.js
@@ -3,8 +3,8 @@
 function setup() {
   // put setup code here
 
-  createCanvas(600, 320);
-  noStroke()
+  createCanvas(480, 120);
+  noStroke();
 }
 
 function draw() {
@@ -12,10 +12,12 @@ function draw() {
 
    background(0);
 
-   for (var y = 0; y <= height; y +=40) {  
-   for (var x = 0; x <= width; x +=40 ) {
+   for (var y = 0; y < height +45; y +=40) {  
      fill (255, 140);
-     ellipse (x, y, 40, 40);
+     ellipse (0, y, 40, 40);
    }
+   for (var x = 0; x < width +45; x +=40 ) {
+     fill (255, 140);
+     ellipse (x, 0, 40, 40);
   }
 }

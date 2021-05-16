5dbdc89d1920955532660364ab1fb5a165da751a
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-16 17:09:01 +0200

    Revisiting the first example

--- chapter-11/sketch.js
+++ chapter-11/sketch.js
@@ -1,15 +1,20 @@
-var x = [12, 2];   //Declare and assign
+var x = [-20, 20];   
 
 
 function setup() {
   // put setup code here
 
-  createCanvas(200, 200);
+  createCanvas(240, 120);
+  noStroke();
 
 }
 
 function draw() {
   // put drawing code here
+  background(0);
+  x[0] += 0.5;  //Increase the first element
+  x[1] += 0.5;  //Increase the second element
+  arc (x[0], 30, 40, 40, 0.52, 5.76);
+  arc (x[1], 90, 40, 40, 0.52, 5.76);
 
-   
 }

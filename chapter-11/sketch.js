4bee691c96e88f0c83d339a65d88033f859845d6
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-16 16:20:42 +0200

    Creating 3000 with an array

--- chapter-11/sketch.js
+++ chapter-11/sketch.js
@@ -1,8 +1,4 @@
-var x1 = -10;
-var x2 = 10;
-var x3 = 35;
-var x4 = 18;
-var x5 = 30;
+var x = [];
 
 
 function setup() {
@@ -10,20 +6,18 @@ function setup() {
 
   createCanvas(240, 120);
   noStroke();
+  fill (255, 200);
+  for (var i = 0; i < 3000; i++)
+    x[i] = random(-1000, 200);
 }
 
 function draw() {
   // put drawing code here
 
    background(0);
-   x1 += 0.5;
-   x2 += 0.5;
-   x3 += 0.5;
-   x4 += 0.5;
-   x5 += 0.5;
-   arc (x1, 20, 20, 20, 0.52, 5.76);
-   arc (x2, 40, 20, 20, 0.52, 5.76);
-   arc (x3, 60, 20, 20, 0.52, 5.76);
-   arc (x4, 80, 20, 20, 0.52, 5.76);
-   arc (x5, 100, 20, 20, 0.52, 5.76);
+   for (var i = 0; i < x.length; i++) {
+      x[i] += 0.5;
+      var y = i*0.4;
+   arc (x[i], y, 12, 12, 0.52, 5.76);
+  }
 }

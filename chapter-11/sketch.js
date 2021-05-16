f0511b8cb4a7ca0bce441c7e6c6e61afaac306fd
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-16 17:20:18 +0200

    Filling an array in a for loop

--- chapter-11/sketch.js
+++ chapter-11/sketch.js
@@ -1,20 +1,20 @@
-var x = [-20, 20];   
+var gray = [];
 
 
 function setup() {
   // put setup code here
 
-  createCanvas(240, 120);
-  noStroke();
-
+  createCanvas(600, 500);
+  for (var i = 0; i < width; i++) {
+    gray[i] = random(0, 255);
+  }
 }
 
 function draw() {
   // put drawing code here
-  background(0);
-  x[0] += 0.5;  //Increase the first element
-  x[1] += 0.5;  //Increase the second element
-  arc (x[0], 30, 40, 40, 0.52, 5.76);
-  arc (x[1], 90, 40, 40, 0.52, 5.76);
-
+  background(204);
+  for (var i = 0; i < gray.length; i++) {
+   stroke (gray[i]);
+   line (i, 0, i, height);
+  }
 }

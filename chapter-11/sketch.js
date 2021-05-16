1e17b8c02778397e9a541ccc67a17f82eb3f6806
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-16 17:44:31 +0200

    Tracking mouse movements using arrays

--- chapter-11/sketch.js
+++ chapter-11/sketch.js
@@ -1,20 +1,31 @@
-var gray = [];
+var num = 60;
+var x = [];
+var y = [];
 
 
 function setup() {
   // put setup code here
 
   createCanvas(600, 500);
-  for (var i = 0; i < width; i++) {
-    gray[i] = random(0, 255);
+  noStroke();
+  
+  for (var i = 0; i < num; i++) {
+    x[i] = 0;
+    y[i] = 0;
   }
 }
-
 function draw() {
   // put drawing code here
-  background(204);
-  for (var i = 0; i < gray.length; i++) {
-   stroke (gray[i]);
-   line (i, 0, i, height);
+  background(0);
+  //Copy array values from back to front
+  for (var i = num - 1; i > 0; i--) {
+    x[i] = x[i-1];
+    y[i] = y[i-1];
   }
+  x[0] = mouseX;  //Set the first element
+  y[0] = mouseY;  //Set the second element
+    for (var i = 0; i < num; i ++) {
+      fill (i*4);
+      ellipse (x[i], y[i], 40, 40);
+    }
 }

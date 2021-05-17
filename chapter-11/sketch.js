fe01e0604584599286998ddb6b7292cc3e4ec308
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-17 21:57:47 +0200

    Jitterbug attempt

--- chapter-11/sketch.js
+++ chapter-11/sketch.js
@@ -1,31 +1,45 @@
-var num = 60;
-var x = [];
-var y = [];
-
+var bugs = [];
 
 function setup() {
   // put setup code here
 
   createCanvas(600, 500);
-  noStroke();
+    background(204);
   
-  for (var i = 0; i < num; i++) {
-    x[i] = 0;
-    y[i] = 0;
+  for (var i = 0; i < 33; i++) {
+    var x = random(width);
+    var y = random(height);
+    var r = i + 2;
+    bugs[i] = new JitterBug(x, y, r);
   }
 }
 function draw() {
   // put drawing code here
-  background(0);
-  //Copy array values from back to front
-  for (var i = num - 1; i > 0; i--) {
-    x[i] = x[i-1];
-    y[i] = y[i-1];
+ 
+  for (var i = 0; i < bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
   }
-  x[0] = mouseX;  //Set the first element
-  y[0] = mouseY;  //Set the second element
-    for (var i = 0; i < num; i ++) {
-      fill (i*4);
-      ellipse (x[i], y[i], 40, 40);
-    }
+
+
+//Copy JitterBug constructor function here
+
+function JitterBug(tempX, tempY, tempDiamter) {
+  this.x = tempX;
+  this.y = tempY;
+  this.diameter =tempDiamter;
+  this.speed = 0.5; //same for every instance
 }
+
+//function move () {
+ // x += random(-speed,speed);
+ // y += random(-speed, speed);
+//}
+
+this.move = function() {
+  this.x += random(-this.speed, this.speed);
+  this.y += random(-this.speed, this.speed);
+};
+this.display = function();
+ ellipse(this.x, this.y, this.diameter, this.diameter);
+};

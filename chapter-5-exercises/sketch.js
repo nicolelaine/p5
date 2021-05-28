3c093a481bbb208ec44c410f9ecdc7003b3f68bb
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-28 20:40:26 +0200

    Exercise 5-6

--- chapter-5-exercises/sketch.js
+++ chapter-5-exercises/sketch.js
@@ -4,12 +4,11 @@ function setup() {
   // put setup code here
 
   createCanvas(480, 120);
-  fill (0, 102);
-  noStroke();
+  strokeWeight(4);
+  stroke(0, 102);
 }
 
 function draw() {
   // put drawing code here
- background (204);
- ellipse (mouseX, mouseY, 9, 9);
+ line (mouseX, mouseY, pmouseX, pmouseY);
 }

3f0ad8f11563bbf8cd049e48482c97d0830e8da5
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-28 20:45:27 +0200

    Exercise 5-7

--- chapter-5-exercises/sketch.js
+++ chapter-5-exercises/sketch.js
@@ -3,12 +3,13 @@
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
-  strokeWeight(4);
+  createCanvas(windowWidth, windowHeight);
   stroke(0, 102);
 }
 
 function draw() {
   // put drawing code here
- line (mouseX, mouseY, pmouseX, pmouseY);
+var weight = dist(mouseX, mouseY, pmouseX, pmouseY);
+strokeWeight (weight);
+line (mouseX, mouseY, pmouseX, pmouseY);
 }

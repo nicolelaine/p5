f767d69342dc8c2671849e842cfd6e30858e9ec9
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-28 19:57:02 +0200

    Example 5-4

--- chapter-5-exercises/sketch.js
+++ chapter-5-exercises/sketch.js
@@ -1,17 +1,15 @@
-var x = 280;
-var y = -100;
-var diameter = 380;
+
 
 function setup() {
   // put setup code here
 
   createCanvas(480, 120);
-  fill (102);
+  fill (0, 102);
+  noStroke();
 }
 
 function draw() {
   // put drawing code here
 
- background (204);
- ellipse (x, y, diameter, diameter);
+ ellipse (mouseX, mouseY, 9, 9);
 }

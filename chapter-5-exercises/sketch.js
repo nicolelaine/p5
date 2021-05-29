8d7d156dfb46d961031042b02dd4838ee71433b2
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-05-29 11:54:05 +0200

    Exercise 5-11

--- chapter-5-exercises/sketch.js
+++ chapter-5-exercises/sketch.js
@@ -14,8 +14,10 @@ background (204);
 stroke(102);
 line (40, 0, 70, height);
 
-if (mouseIsPressed === true) {
+if (mouseIsPressed) {
   stroke(0);
+} else {
+  stroke(255);
 }
    line (0, 70, width, 50);
 }

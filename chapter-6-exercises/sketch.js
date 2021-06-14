d32ea9fb8741cab2989efae0d0ee1a3a5cb0a76b
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-14 21:27:33 +0200

    Added robot 4 - translate, rotate, scale

--- chapter-6-exercises/sketch.js
+++ chapter-6-exercises/sketch.js
@@ -1,20 +1,62 @@
+var x = 60;  //x coordinate
+var y = 440;  //y coordinate
+var radius = 45;  //head radius
+var bodyHeight = 180;  //body height
+var neckHeight = 40;  //Neck height
 
+var easing = 0.04;
 
 function setup() {
   // put setup code here
 
   createCanvas(windowWidth, windowHeight);
-  background (204)
- 
+  strokeWeight (2);
+  ellipseMode (RADIUS);
 }
 
 function draw() {
   // put drawing code here
   
-  push ()
-  translate (mouseX, mouseY);
-  rect (0, 0, 30, 30);
-  pop ();
-  translate (35, 10);
-  rect (0, 0, 15, 15);
+ var neckY = -1 * (bodyHeight + neckHeight + radius);
+
+ background (204);
+
+ translate (mouseX, y);  //Move all to (mouseX, y)
+
+ if (mouseIsPressed) {
+   scale (1.0);
+ }  else {
+   scale (0.6);  //60% size when mouse is pressed
+ }
+
+ //Body
+ noStroke ();
+ fill (102);
+ ellipse (0, -33, 33, 33);
+ fill (0);
+ rect (-45, -bodyHeight, 90, bodyHeight-33);
+
+ //Neck
+ stroke (102);
+ line (12, -bodyHeight, 12, neckY);
+
+ //Hair
+ push ()
+ translate (12, neckY);
+ var angle = -PI/30.0;
+ for (var i = 0; i <= 30; i++) {
+   line (80, 0, 0, 0);
+   rotate (angle);
+ }
+ pop ();
+
+//Head 
+noStroke ();
+fill (0);
+ellipse (12, neckY, radius, radius);
+fill (255);
+ellipse (24, neckY-6, 14, 14);
+fill (0);
+ellipse (24, neckY-6, 3, 3);
+
 }

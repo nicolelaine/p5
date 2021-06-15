9b5c3046876a04cd667e1709c0e6070c2f2f9d27
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-15 12:00:09 +0200

    Added edX exercise with translate and yellow rectangle

--- translate-examples/sketch.js
+++ translate-examples/sketch.js
@@ -2,18 +2,24 @@ function setup() {
   // put setup code here
 
   createCanvas(400, 400);
+  colorMode (HSB, 360, 100, 100);
 }
 
 function draw() {
   // put drawing code here
 
-  background (220);
-  noStroke ();  // no outline on each rectangle
-
-  fill (255, 0, 0);
-  rect (0, 0, width/2, height/2);
+  background (200, 100, 30);
   
-  translate (width/2, height/2);
-  fill (0, 255, 0);
-  rect (0, 0, width/2, height/2);
+  fill (47, 76, 100);
+
+  push ();
+  translate (mouseX, mouseY);
+  triangle (-40, 0, 40, 0, 0, -40);
+  pop ();
+  
+  drawCoords ();
+}
+
+function drawCoords () {
+    text (mouseX + ":" + mouseY, 20, height-45);
 }

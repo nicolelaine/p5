e89e84e1f9357b0db46c3b3bf01c3c6c77f975f6
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 13:45:20 +0200

    Added example 8-15, spirals

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,11 +1,12 @@
 var angle = 0.0;
 var offset = 60;
-var scalar = 30;
+var scalar = 2;
 var speed = 0.05;
 
 function setup() {
   // put setup code here
   createCanvas (240, 120);
+  fill (0);
   background (204);
 }
 
@@ -14,6 +15,7 @@ function draw() {
 
  var x = offset + cos (angle) * scalar;
  var y = offset + sin (angle) * scalar;
- ellipse (x, y, 40, 40);
+ ellipse (x, y, 2, 2);
  angle += speed;
+ scalar += speed;
 }

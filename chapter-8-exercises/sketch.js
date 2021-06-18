d5ac3d0a45c40b79235abb873971de43bef0ee2d
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 12:08:09 +0200

    Added example 8-11, triggering timed events, ball rolls off screen to the left at a certain time

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,13 +1,19 @@
-
+var time1 = 2000;
+var time2 = 4000;
+var x = 0;
 
 function setup() {
   // put setup code here
-   
+   createCanvas (480, 120);
 }
 
 function draw() {
   // put drawing code here
 
- var timer = millis ();
- print (timer);
+ var currentTime = millis ();
+ background (204);
+    if (currentTime > time2) {
+      x -= 2;
+    }
+    ellipse (x, 60, 90, 90);
 }

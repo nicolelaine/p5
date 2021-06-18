93c49d36ee5beb6052a84eb118ba3d88ada2ce32
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 13:42:05 +0200

    Added example 8-14, circular motion

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,22 +1,19 @@
 var angle = 0.0;
 var offset = 60;
-var scalar = 40;
+var scalar = 30;
 var speed = 0.05;
 
 function setup() {
   // put setup code here
   createCanvas (240, 120);
+  background (204);
 }
 
 function draw() {
   // put drawing code here
 
- background (0);
- var y1 = offset + sin (angle) * scalar;
- var y2 = offset + sin (angle + 0.4) * scalar;
- var y3 = offset + sin (angle + 0.8) * scalar;
- ellipse (80, y1, 40, 40);
- ellipse (120, y2, 40, 40);
- ellipse (160, y3, 40, 40);
+ var x = offset + cos (angle) * scalar;
+ var y = offset + sin (angle) * scalar;
+ ellipse (x, y, 40, 40);
  angle += speed;
 }

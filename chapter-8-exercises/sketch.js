0b991395c0f509cbf7f355afc5fc84f36addd29a
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 11:59:47 +0200

    Added example 8-9, part 1, move shapes randomly

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,18 +1,20 @@
-
+var speed = 2.5;
+var diameter = 20;
+var x;
+var y;
 
 function setup() {
   // put setup code here
    createCanvas (240, 120);
+   x = width/2;
+   y = height/2;
+   background (204);
 }
 
 function draw() {
   // put drawing code here
 
-background (204);
-for (var x = 20; x < width; x+=20) {
-  var mx = mouseX / 10;
-  var offsetA = random(-mx, mx);
-  var offsetB = random(-mx, mx);
-  line (x + offsetA, 20, x - offsetB, 100);
-}
+  x += random (-speed, speed);
+  y += random (-speed, speed);
+     ellipse (x, y, diameter, diameter);
 }

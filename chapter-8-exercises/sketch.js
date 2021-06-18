e888cc5a033f313da3e8e3fcf71e26c73dabb06f
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 11:43:45 +0200

    Added example 8-8, draw randomly

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -2,12 +2,17 @@
 
 function setup() {
   // put setup code here
- 
+   createCanvas (240, 120);
 }
 
 function draw() {
   // put drawing code here
 
-var r = random (0, mouseX);
-print (r);
+background (204);
+for (var x = 20; x < width; x+=20) {
+  var mx = mouseX / 10;
+  var offsetA = random(-mx, mx);
+  var offsetB = random(-mx, mx);
+  line (x + offsetA, 20, x - offsetB, 100);
+}
 }

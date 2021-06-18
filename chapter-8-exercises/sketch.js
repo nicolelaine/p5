2fb5fe1a5134989d6bd2198e2f6f83632ac9ea75
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 13:38:46 +0200

    Added example 8-13, sine wave movement

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,16 +1,22 @@
 var angle = 0.0;
+var offset = 60;
+var scalar = 40;
+var speed = 0.05;
 
 function setup() {
   // put setup code here
+  createCanvas (240, 120);
 }
 
 function draw() {
   // put drawing code here
 
- var sinval = sin(angle);
- print (sinval);
-
- var gray = map(sinval, -1, 1, 0, 255);
- background (gray);
- angle += 0.1;
+ background (0);
+ var y1 = offset + sin (angle) * scalar;
+ var y2 = offset + sin (angle + 0.4) * scalar;
+ var y3 = offset + sin (angle + 0.8) * scalar;
+ ellipse (80, y1, 40, 40);
+ ellipse (120, y2, 40, 40);
+ ellipse (160, y3, 40, 40);
+ angle += speed;
 }

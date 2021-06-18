ba6b7c1ffa73b4e54ccf455d47ff4b6089696a45
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-18 12:05:00 +0200

    Added example 8-10, time passes

--- chapter-8-exercises/sketch.js
+++ chapter-8-exercises/sketch.js
@@ -1,24 +1,13 @@
-var speed = 2.5;
-var diameter = 20;
-var x;
-var y;
+
 
 function setup() {
   // put setup code here
-   createCanvas (240, 120);
-   x = width/2;
-   y = height/2;
-   background (204);
+   
 }
 
 function draw() {
   // put drawing code here
 
-  x += random (-speed, speed);
-  y += random (-speed, speed);
-  
-  x = constrain (x, 0, width);
-  y = constrain (y, 0, height);
-  
-     ellipse (x, y, diameter, diameter);
+ var timer = millis ();
+ print (timer);
 }

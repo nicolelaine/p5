4740222bafe24f2c6ed7d0e936ad729a32fd5355
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-20 21:26:32 +0200

    Simple harmonic motion

--- sine-cosine-exercises/sketch.js
+++ sine-cosine-exercises/sketch.js
@@ -1,24 +1,29 @@
+//simple harmonic motion
+
 var angle = 0.0;
+var x = 0.0;
+var period = 125;  //sets the speed of the object as it moves across the screen
+
 
 
 function setup() {
   // put setup code here
 
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // put drawing code here
 
-   //background (255);
-   translate (width / 2, height /2);
-   var amplitude = height / 4; //play with the altitude
+   background (255);
+   fill (0);
+   text ("period: " + period, 175, 30);
+   translate (width / 2, height / 2);
+   var amplitude = width / 2;  //play with the amplitude
+   var angle = frameCount / period * TWO_PI;  //as the period number gets smaller
+   //it moves more quickly
+   x = cos (angle) * amplitude;
 
-   angle += 2.0;
-   var x, y;
-   y = sin (radians(angle)) * amplitude;
-   // try adding an ofset to our angle here
-   x = cos (radians(angle + 45))  * amplitude;
-   fill (100);
-   ellipse (x, y, 10, 10);
+   fill (245, 0, 123);
+   rect (x, 0, 40, 40);
 }

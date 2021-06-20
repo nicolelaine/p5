5dbe8c66fc8be42ee2d6c196864ad8c8eb9ef141
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-20 14:02:37 +0200

    Sine wave graphed

--- math-behind-sine-and-cosine-functions/sketch.js
+++ math-behind-sine-and-cosine-functions/sketch.js
@@ -1,14 +1,10 @@
-var xspeed = 10.0;
-var x = 0.0;
-var angle = 0.0;
-
 
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  frameRate (10);
+  
 
 }
 
@@ -16,17 +12,16 @@ function draw() {
   // put drawing code here
 
    background (255);
+   var angle = 0.0;
+   var x = 0.0;
    translate (0, height / 2);
-   var amplitude = height / 4; //play with the amplitude
-   var y;
-   if (x === 0) {
-      angle = 0;
-   }  else {
-      angle += 10.0;
-   }
-  y = sin (radians (angle)) * amplitude;
-   fill (100);
-   ellipse (x, y, 10, 10);
-   x += 10.0;
-    x = x % width;
+
+     while (x < width) {
+      var amplitude = height / 4; //play with the amplitude
+      var y = sin (radians (angle)) * amplitude;
+      fill (100);
+      ellipse (x, y, 10, 10);
+      x += 10.0;
+       angle += 10.0;
+     }
 }

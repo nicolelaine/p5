b666a048acb64fcfc694b446c375edaaf5f7642a
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-20 13:30:00 +0200

    Making a ball bounce using elastic motion

--- math-behind-sine-and-cosine-functions/sketch.js
+++ math-behind-sine-and-cosine-functions/sketch.js
@@ -1,16 +1,16 @@
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
-  frameRate (10);
+  createCanvas(400, 400);
+
 }
 
 function draw() {
   // put drawing code here
 
    background (255);
-   var val = cos (radians(frameCount)) *34;
-   print (val);
-
-   //shows values by negative 1 and 1
+   var amplitude = height / 2; //play with the amplitude
+   var y = sin (radians (frameCount)) * amplitude;
+   translate (width / 2, height / 2)
+   ellipse (0, y, 50, 50);
 }

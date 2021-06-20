dcfa225bcd38124d6ed4220651e0ce904ce5ed4e
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-20 20:55:02 +0200

    Sine Cosine yo-yo ball

---
+++ sine-cosine-exercises/sketch.js
@@ -0,0 +1,24 @@
+var angle = 0.0;
+
+
+function setup() {
+  // put setup code here
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // put drawing code here
+
+   background (255);
+   translate (width / 2, height /2);
+   var amplitude = height / 4; //play with the altitude
+
+   angle += 2.0;
+   var x, y;
+   y = sin (radians(angle)) * amplitude;
+   // try adding an ofset to our angle here
+   x = sin (radians(angle))  * amplitude;
+   fill (100);
+   ellipse (x, y, 10, 10);
+}

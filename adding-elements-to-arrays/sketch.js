8559d0e46a508e67711e1f8a3abfb16946ee90fa
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-21 21:46:25 +0200

    Using shorten () to remove the last element of an arry

--- adding-elements-to-arrays/sketch.js
+++ adding-elements-to-arrays/sketch.js
@@ -1,26 +1,28 @@
-var mySecondArray = [];
+var myThirdArray = [
+  "apple",
+  "orange",
+  "banana",
+  "peach"
+];
 
 function setup() {
   // put setup code here
 
   createCanvas(500, 500);
 
-  print (mySecondArray);
-  for (var i = 0; i < 5; i++) {
-    mySecondArray[i] = i * 50;
   }
   
-  print (mySecondArray);
-}
 
 function draw() {
   // put drawing code here
 
    background (255);
-   noFill ();
-   for (var i = 0; i < mySecondArray.length; i++) {
-     ellipse (width / 2, height / 2, mySecondArray [i] + 50, mySecondArray [i] + 50);
-     //print ("element value is" + myFirstArray[i]);
-   }
+     if (myThirdArray.length === 0) {
+       noLoop ();
+     }
+       text (myThirdArray[ myThirdArray.length - 1], width / 2, height / 2);
 }
 
+function mousePressed () {
+   shorten (myThirdArray);
+}

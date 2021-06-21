154985cddc7293ad7cda962929978d9e982b4082
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-21 21:05:48 +0200

    Making a bullseye drawing with adding numbers to an array

--- adding-elements-to-arrays/sketch.js
+++ adding-elements-to-arrays/sketch.js
@@ -1,29 +1,26 @@
-var myFirstArray = [];
+var mySecondArray = [];
 
 function setup() {
   // put setup code here
 
   createCanvas(500, 500);
-  colorMode (HSB, 360, 100, 100, 100);
- 
+
+  print (mySecondArray);
   for (var i = 0; i < 5; i++) {
-    myFirstArray.push(random(100));
+    mySecondArray[i] = i * 50;
   }
-  rectMode (CENTER);
-  print (myFirstArray);
+  
+  print (mySecondArray);
 }
 
 function draw() {
   // put drawing code here
 
-   background (0, 0, 100);
-   for (var i = 0; i < myFirstArray.length; i++) {
-     fill (0, 100, 100, 0.4);
-     rect (width / 2, height / 2, myFirstArray [i] * 10, myFirstArray [i] * 10);
+   background (255);
+   noFill ();
+   for (var i = 0; i < mySecondArray.length; i++) {
+     ellipse (width / 2, height / 2, mySecondArray [i] + 50, mySecondArray [i] + 50);
      //print ("element value is" + myFirstArray[i]);
    }
 }
 
-function mousePressed () {
-
-}

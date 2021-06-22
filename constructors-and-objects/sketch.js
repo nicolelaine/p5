dd210a4af2bb3301ad243e6fdd1a87763795e6bd
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-22 14:26:39 +0200

    Added second exercise with many balls bouncing and fading out

--- constructors-and-objects/sketch.js
+++ constructors-and-objects/sketch.js
@@ -1,40 +1,57 @@
 class Particle {   //making the cookie cutter
    constructor (pos) {
-   this.name = "ball";
+   this.name = "astroid";
    this.color = "red";
-   this.pos = pos; //p5 vector
-   this.speed = 1;
+   this.pos = pos; //pos is a p5 vector
+   this.speed = 10;
+   this.lifespan = 255;
   }
   bounce () {  //this is now a method
+    //if particle position has gone outside the bondary
+    //of our canvas = reverse speed!
     if (this.pos.y >= height || this.pos.y <= 0) {  //this keeps in on the screen
          this.speed *= -1;
     }
-    this.pos.y += this.speed; //it's the y because this is a vector
+    //add speed to our particle postion
+    this.pos.add(new p5.Vector(0, this.speed));
+    if (this.lifespan > 0) {
+      this.lifespan--;
+    }
   }
 }
 
-var ball;
+var particles = [];
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  ball = new Particle (createVector(random(width), random(height))); //this creates a new instance 
-  //of the particle - makes the  cookie, so to say
-  //var particle = new Particle (createVector(random(width), random(height))); //this creates a new instance 
-  //of the particle - makes the  cookie, so to say
- // print (particle.pos);  //this instance of the particle
- textAlign (CENTER);
+  particles.push(new Particle(createVector(width/2, height/2)));
 }
 
 function draw() {
   // put drawing code here
-  background (255);
-  fill (ball.color);
-  ellipse (ball.pos.x, ball.pos.y, 50, 50);
-  fill (255);
-  text (ball.name, ball.pos.x, ball.pos.y);
-  ball.bounce (); //this calls the bounce method
 
-  
+  background (255);
+  for (var i = 0; i < particles.length; i++) {
+    var particle = particles[i];
+        if (particle.lifespan <= 0) {
+              particles.splice(i, 1);
+              break;
+        }  else {
+    var particleColor = lerpColor (color(255), color (particle.color), particle.lifespan / 255);
+        fill (particleColor);
+        noStroke ();
+        ellipse (particle.pos.x, particle.pos.y, 100, 100);
+
+        fill (255); //white text
+        textAlign (CENTER);
+        text (particle.name, particle.pos.x, particle.pos.y);
+        particle.bounce ();
+  }
+  }
+}
+
+function mousePressed () {
+  particles.push(new Particle(createVector(width/2, height/2)));
 }

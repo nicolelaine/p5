b0375bdc0dd4b0e04731a7274c9016e9ee62c1f1
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-28 14:18:39 +0200

    Added listen to mic exercise

---
+++ chapter-13-exercises/listen-to-mic.js
@@ -0,0 +1,34 @@
+var mic;
+var amp;
+
+var scale = 1.0;
+
+
+function setup() {
+  // put setup code here
+
+  createCanvas(440, 440);
+  background (0);
+  //Create an audio input and start it
+  mic = new p5.AudioIn ();
+  mic.start ();
+  //Create a new amplitude analyzer and patch into input
+  amp = new p5.Amplitude ();
+  amp.setInput(mic);
+}
+
+function draw() {
+  // put drawing code here
+
+  //Draw a background that fades to black
+  noStroke ();
+  fill (0, 10);
+  rect (0, 0, width / height);
+  //The getLevel () method returns values
+  //between 0 and 1, so map () is used to
+  //convert the values to larger numbers
+  scale = map (amp.getLevel(), 0, 1.0, 10, width);
+  //Draw the circle based on the volume
+  fill (255);
+  ellipse (width / 2, height / 2, scale, scale);
+}

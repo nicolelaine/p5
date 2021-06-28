9aa7b5625c0f0fdea390817c732fa275c203e8c2
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-28 13:24:20 +0200

    Added exercise 13.1

--- chapter-13-exercises/sketch.js
+++ chapter-13-exercises/sketch.js
@@ -1,16 +1,35 @@
+var blip;
+
+var radius = 120;
+var x = 0;
+var speed = 1.0;
+var direction = 1;
+
+function preload () {
+  blip = loadSound ("blip.wav");
+}
+
+
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
+  createCanvas(440, 440);
+  ellipseMode (RADIUS);
+  X = width/2 //start in the center
 }
 
 function draw() {
   // put drawing code here
 
-   if(mouseIsPressed) {
-    fill(0);
-      } else {
-        fill(255);
+   background (0);
+   x += speed * direction;
+      if ((x > width - radius) || (x < radius)) {
+           direction = -direction //flip direction
+           blip.play();
       }
-      ellipse(mouseX, mouseY, 80, 80);
+   if (direction === 1) {
+         arc (x, 220, radius, radius, 0.52, 5.76); //face right
+   }  else {
+     arc (x, 220, radius, radius, 3.67, 8.9); //face left
+   }
 }

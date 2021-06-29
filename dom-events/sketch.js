f7dcf320fd1837aaf3c058efa3c24f3fc351260a
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-06-29 17:55:07 +0200

    Input elements exercise with t

--- dom-events/sketch.js
+++ dom-events/sketch.js
@@ -1,22 +1,32 @@
-var btn;
-var bg = "black";
+var inputTextElement;  //placeholder for the dom element
+var inputText = '';
 
 
 function setup() {
   // put setup code here
 
-  createCanvas(windowWidth, windowHeight);
-  btn = createElement ('button', 'Click me');
-  btn.position(20,20);
-  btn.mousePressed (
-    function () {  //does not have a name, makes it an anonymous function
-      bg = color (random(255), random(255), random(255))
-    }
-  );
+  createCanvas(500, 500);
+
+  inputTextElement = createInput ();
+  inputTextElement.position (20, 20);
+  inputTextElement.input(handleInput);  //custom callback function
+
+
 }
 
-function draw() {
+function handleInput () {
+  inputText = this.value (); //refers to the inputTextElement, which is an object
+}
+
+
+
+function keyPressed () {
+  if (key === 't') {
+    text (inputText, random(width), random(height));
+  }
+}
+
+//function draw() {
   // put drawing code here
 
-  background (bg);
-}
+//}

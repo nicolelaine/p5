b0b0701be4377931590adaf26a792acc4904d927
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 13:24:26 +0200

    Added the exercise for get pos tags using riTa

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,12 +1,18 @@
-var lyricline = "Thank you very much Mister Robot"
+//Couplet by Ezra Pound
+
+var couplet = "Drawing a sword, cut into water, water again flow: Raise cup, quench sorrow again sorry."
+
 var words = [];
-var stresses;
+var partOfSpeech = [];
+
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  textSize (20);
+  textSize (12);
+  words = RiTa.tokenize(couplet);
+  partOfSpeech = RiTa.getPosTags(words); //parts of speech tags
   
   //get all the stresses as an array
 
@@ -18,13 +24,17 @@ function draw() {
   // put drawing code here
 
   background (255);
-  translate (20, 20);
-  var wordPos = 0;
+  translate (10, 10);
+  var wordPosX = 0;
+  var wordPosY = width / 4;
+
   for (var i = 0; i < words.length; i++) {
-   // text (words[i], wordPos, 0, textWidth(words))
-    var stresses = RiTa.getStresses(words)
-    text (words[i], wordPos, 20)
-    text(stresses, wordPos, 45)
-    wordPos += textWidth(words[i]);
+    text (words[i], wordPosX, wordPosY, textWidth(words[i]), 20);
+    text (partOfSpeech[i], wordPosX, wordPosY + 20);
+    wordPosX += textWidth(words[i]) + 16; //gives some space in between the words
+       if (wordPosX > width) {
+         wordPosX = 0;
+         wordPosY += 35;  // the 35 gives some space in between the words
+       }
   }
 }

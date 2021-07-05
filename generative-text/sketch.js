e5d229dda4f149d1e495fc23f0f440879bee3237
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 10:55:51 +0200

    Added riTa tokenize exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,16 +1,17 @@
 function setup() {
   // put setup code here
 
-  createCanvas(480, 120);
+  createCanvas(400, 400);
+  textSize (20);
+  noStroke ();
+
+  //take a string and split it into an array of words
+   var words = riTa.tokenize("I scream you scream we all scream for ice cream");
+      for (var i = 0, i = words.length; i < j; i++)  {
+             text(words[i], 10, i * 20 + 20);
+         
 }
 
 function draw() {
   // put drawing code here
-
-   if(mouseIsPressed) {
-    fill(0);
-      } else {
-        fill(255);
-      }
-      ellipse(mouseX, mouseY, 80, 80);
 }

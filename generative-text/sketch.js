7dd4361eced305b00c3a2522de7122cd06fa907e
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 12:20:52 +0200

    Added get stresses riTa exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,26 +1,30 @@
+var lyricline = "Thank you very much Mister Robot"
+var words = [];
+var stresses;
+
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
   textSize (20);
   
-  var word = "huge";
-  var wordWidth = textWidth (word);  //this gives us the number of pixels the word is
-  text (word, 50, 50, wordWidth, 100);  //where the text is placed
-  rect (50, 70, wordWidth, wordWidth);  //a rectangle for under the word to show its size below it
+  //get all the stresses as an array
 
-
-  var wordTwo = "huuuuuge";
-  var wordTwoWidth = textWidth (wordTwo);
-  text (wordTwo, 100 + wordWidth, 50, wordTwoWidth, 20);
-  rect (100 + wordWidth, 70, wordTwoWidth, wordTwoWidth);
-
-  var wordThree = "huuuuuuuuuuuuuuuuge";
-  var wordThreeWidth = textWidth (wordThree);
-  text (wordThree, 150 + wordTwoWidth, 50, wordThreeWidth, 20);
-  rect (150 + wordTwoWidth, 70, wordThreeWidth, wordThreeWidth);
+  stresses = RiTa.getStresses(lyricline);
+  words = RiTa.tokenize(lyricline);
 }
 
 function draw() {
   // put drawing code here
+
+  background (255);
+  translate (20, 20);
+  var wordPos = 0;
+  for (var i = 0; i < words.length; i++) {
+   // text (words[i], wordPos, 0, textWidth(words))
+    var stresses = RiTa.getStresses(words)
+    text (words[i], wordPos, 20)
+    text(stresses, wordPos, 45)
+    wordPos += textWidth(words[i]);
+  }
 }

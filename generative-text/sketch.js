1861b1d0bbcce91efd774d544e3bf0c99aceb7c2
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 11:57:35 +0200

    Added textWidth exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -4,18 +4,21 @@ function setup() {
   createCanvas(400, 400);
   textSize (20);
   
-  noFill ();
-  stroke (2);
-  rect (25, 25, 350, 350);
+  var word = "huge";
+  var wordWidth = textWidth (word);  //this gives us the number of pixels the word is
+  text (word, 50, 50, wordWidth, 100);  //where the text is placed
+  rect (50, 70, wordWidth, wordWidth);  //a rectangle for under the word to show its size below it
 
-  noStroke ();
-  fill (0);
 
-  //gets the phonemes from a string of text
-   var words = riTa.getPhonemes("Got me looking so crazy right now");
-      text (words, 50, 50, 300, 100);
+  var wordTwo = "huuuuuge";
+  var wordTwoWidth = textWidth (wordTwo);
+  text (wordTwo, 100 + wordWidth, 50, wordTwoWidth, 20);
+  rect (100 + wordWidth, 70, wordTwoWidth, wordTwoWidth);
 
-      //you could do words.length to show many many sounds (phonetic words) in in the words
+  var wordThree = "huuuuuuuuuuuuuuuuge";
+  var wordThreeWidth = textWidth (wordThree);
+  text (wordThree, 150 + wordTwoWidth, 50, wordThreeWidth, 20);
+  rect (150 + wordTwoWidth, 70, wordThreeWidth, wordThreeWidth);
 }
 
 function draw() {

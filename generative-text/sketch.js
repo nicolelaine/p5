471ab298e597dbce28ed78c0d3b91f491b0197c7
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 23:13:08 +0200

    Added Rilexicon exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,21 +1,27 @@
-var lyricline = "Thank you very much Mister Robot"
 
+var lexicon;
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  textSize (120);
+
+  textSize (20);
   textAlign (CENTER);
+  text ("Click for random word", width / 2, height / 2);
+
+    //first create new Rilexicon - an instance of this object
+    lexicon = new Rilexicon ();
+}
+
+function mousePressed () {
+   background (255);
+   var word = lexicon.random ();
+   //get a random word from our lexicon object
+   text (word, width/2, height/2);
 }
 
 function draw() {
   // put drawing code here
-
-  background (255);
-  
-      text (lyricline, 0, height / 2, width, 20);
-
-      text (RiTa.getWordCount(lyricline), height / 2, height / 2 + 45);
   
 }

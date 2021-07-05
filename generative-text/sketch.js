250ec74f286fb878d648206f9d3fc8b853b5ff05
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-05 13:30:28 +0200

    Added RiTa get word count exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,40 +1,21 @@
-//Couplet by Ezra Pound
-
-var couplet = "Drawing a sword, cut into water, water again flow: Raise cup, quench sorrow again sorry."
-
-var words = [];
-var partOfSpeech = [];
+var lyricline = "Thank you very much Mister Robot"
 
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  textSize (12);
-  words = RiTa.tokenize(couplet);
-  partOfSpeech = RiTa.getPosTags(words); //parts of speech tags
-  
-  //get all the stresses as an array
-
-  stresses = RiTa.getStresses(lyricline);
-  words = RiTa.tokenize(lyricline);
+  textSize (120);
+  textAlign (CENTER);
 }
 
 function draw() {
   // put drawing code here
 
   background (255);
-  translate (10, 10);
-  var wordPosX = 0;
-  var wordPosY = width / 4;
+  
+      text (lyricline, 0, height / 2, width, 20);
 
-  for (var i = 0; i < words.length; i++) {
-    text (words[i], wordPosX, wordPosY, textWidth(words[i]), 20);
-    text (partOfSpeech[i], wordPosX, wordPosY + 20);
-    wordPosX += textWidth(words[i]) + 16; //gives some space in between the words
-       if (wordPosX > width) {
-         wordPosX = 0;
-         wordPosY += 35;  // the 35 gives some space in between the words
-       }
-  }
+      text (RiTa.getWordCount(lyricline), height / 2, height / 2 + 45);
+  
 }

79e28e66596506951378e4ba615cc6ba578ab925
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-06 13:08:49 +0200

    Added preLoad and loadStrings example

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,40 +1,19 @@
 
-var lexicon;
+var lines;
+
+function preLoad () {
+   lines = loadStrings('assets/The Problems of Philosphy.txt')  //you need to have this file in your folder!
+    //it will add all the text in this document in an array of lines
+  }
 
 function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-  lexicon = new Rilexicon ();   //first create new Rilexicon - an instance of this object
-  textSize (20);
-  textAlign (CENTER, CENTER); //vertical and horizontal center
-  generateHaiku ();
+  text (lines.join(' '), 0, 0, width, height); //the joins puts the lines together
 }
 
-function mousePressed () {
-   background (255);
-   generateHaiku ();
-}
 
-function generateHaiku () {
-    //5 syllables
-    var output = lexicon.randomWord ('jj, 1') + '';  //this makes a space at the end
-        output += lexicon.randomWord ('vbg, 2') + '';
-        output += lexicon.randomWord ('nn, 2') + '/n';  //this makes a new line!
-
-    //7 syllables
-        output += lexicon.randomWord ('vbg, 2') + '';  //verb gerundive
-        output += lexicon.randomWord ('jj, 2') + '';  ///adjective
-        output += lexicon.randomWord ('nn, 3') + '/n';  //this makes a new line! and nn is a noun
-
-
-     //5 syllables
-        output += lexicon.randomWord ('nns, 1') + '';  //verb gerundive  (nns = noun plural)
-        output += lexicon.randomWord ('rb, 2') + '';  ///adverb
-        output += lexicon.randomWord ('vbg, 2') + '/n';  //this makes a new line! and nn is a noun
-
-       text (output, width / 2, height / 2);
-}
 
 //function draw() {
   // put drawing code here

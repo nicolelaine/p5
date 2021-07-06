3b94f8172d100a1924ce6436c47aeb656e4b8a17
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-06 12:19:11 +0200

    Added the haiku generator exercise

--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -5,23 +5,38 @@ function setup() {
   // put setup code here
 
   createCanvas(400, 400);
-
+  lexicon = new Rilexicon ();   //first create new Rilexicon - an instance of this object
   textSize (20);
-  textAlign (CENTER);
-  text ("Click for random word", width / 2, height / 2);
-
-    //first create new Rilexicon - an instance of this object
-    lexicon = new Rilexicon ();
+  textAlign (CENTER, CENTER); //vertical and horizontal center
+  generateHaiku ();
 }
 
 function mousePressed () {
    background (255);
-   var word = lexicon.random ();
-   //get a random word from our lexicon object
-   text (word, width/2, height/2);
+   generateHaiku ();
 }
 
-function draw() {
+function generateHaiku () {
+    //5 syllables
+    var output = lexicon.randomWord ('jj, 1') + '';  //this makes a space at the end
+        output += lexicon.randomWord ('vbg, 2') + '';
+        output += lexicon.randomWord ('nn, 2') + '/n';  //this makes a new line!
+
+    //7 syllables
+        output += lexicon.randomWord ('vbg, 2') + '';  //verb gerundive
+        output += lexicon.randomWord ('jj, 2') + '';  ///adjective
+        output += lexicon.randomWord ('nn, 3') + '/n';  //this makes a new line! and nn is a noun
+
+
+     //5 syllables
+        output += lexicon.randomWord ('nns, 1') + '';  //verb gerundive  (nns = noun plural)
+        output += lexicon.randomWord ('rb, 2') + '';  ///adverb
+        output += lexicon.randomWord ('vbg, 2') + '/n';  //this makes a new line! and nn is a noun
+
+       text (output, width / 2, height / 2);
+}
+
+//function draw() {
   // put drawing code here
   
-}
+//}

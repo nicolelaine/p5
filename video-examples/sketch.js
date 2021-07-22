be847fbc72f59ceb4d1a12ea213403eb6e4f23e2
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-22 11:13:26 +0200

    Added bubble addCue exercise

--- video-examples/sketch.js
+++ video-examples/sketch.js
@@ -1,5 +1,5 @@
 /*
-* @name Hello World Video
+* @name Video Cues
 * @description <p> load a video and create a DOM element for it to play inside </p>
 * <p><em><span class = "small"> To run this example locally, you will need the
 * <a href="http://p5js.org/reference/#/libraries/p5.dom">p5.dom library</a>
@@ -8,9 +8,13 @@
 */
 
 var space;
+var bgColor;
+var isMakeBubble = false;
+var bubblePos;
 
 function setup() {
   // put setup code here
+  bgColor = color(255, 255, 255);
 
   createCanvas(windowWidth, windowHeight);
   //specify multiple formats for different browsers
@@ -24,6 +28,9 @@ function setup() {
   space.postion (width/2 - space.width/2, height / 2 - space.height/2);
 
   textSize (32);
+
+  space.addCue(3.0, changeBgColor);
+  space.addCue(4.0, triggerBubble, random(height));
 }
 
 function draw() {
@@ -32,4 +39,23 @@ function draw() {
    background (0);
    text("current time is " + space.time(), 20, 32); //it's called space, as that's the variable in this case for the video. the numbers show it's location on screen
    text("video duration in seconds is " + space.duration(), 20, 64);
+
+   if (isMakeBubble) { //we don't need to say true, since if it is, it will run the code
+          if(bubblePos < 0) {
+            isMakeBubble = false;
+          }
+      else {
+        ellipse(width/2, bubblePos, 20, 20);
+        bubblePos--;
+      }
+   }
+}
+
+function changeBgColor () {
+  bgColor = color(0, 255, 0);
+}
+
+function triggerBubble (posY) { //position Y
+    isMakeBubble = true;
+    bubblePos = posY;
 }

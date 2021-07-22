62343b5aa2215233629cc57f6f8b2af6503959dc
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-07-22 14:19:13 +0200

    Added exercise of drawing the video to the canvas - not in a DOM element

--- video-examples/sketch.js
+++ video-examples/sketch.js
@@ -8,54 +8,34 @@
 */
 
 var space;
-var bgColor;
-var isMakeBubble = false;
-var bubblePos;
+
+function preload ();
+space = createVideo (["media/anni003.mp4"]);  //video DOM element with an array inside with a string to where the video lives
+
 
 function setup() {
   // put setup code here
-  bgColor = color(255, 255, 255);
+
 
   createCanvas(windowWidth, windowHeight);
   //specify multiple formats for different browsers
   //you can also create a backup video format in case mp4 does not work by putting it next in the array
-  space = createVideo (["media/anni003.mp4"]);  //video DOM element with an array inside with a string to where the video lives
+  
   space.loop (); //set the video to loop 
-  space.volume (); //we set the volume to 0 to mute it
 
-  space.showControls (true);
-  //a centered video element
-  space.postion (width/2 - space.width/2, height / 2 - space.height/2);
+  space.hide() //by default hides the DOM element
+  //this makes it play inside of the canvas and not as a separate DOM element
 
-  textSize (32);
+  space.volume (0); //we set the volume to 0 as we don't want sound
 
-  space.addCue(3.0, changeBgColor);
-  space.addCue(4.0, triggerBubble, random(height));
+  
 }
 
 function draw() {
   // put drawing code here
 
-   background (0);
-   text("current time is " + space.time(), 20, 32); //it's called space, as that's the variable in this case for the video. the numbers show it's location on screen
-   text("video duration in seconds is " + space.duration(), 20, 64);
-
-   if (isMakeBubble) { //we don't need to say true, since if it is, it will run the code
-          if(bubblePos < 0) {
-            isMakeBubble = false;
-          }
-      else {
-        ellipse(width/2, bubblePos, 20, 20);
-        bubblePos--;
-      }
-   }
-}
-
-function changeBgColor () {
-  bgColor = color(0, 255, 0);
-}
-
-function triggerBubble (posY) { //position Y
-    isMakeBubble = true;
-    bubblePos = posY;
+   background (150);
+   tint (255, 0, 0); //this tints the video as if it were an image
+   image (space, width / 2 - space.width / 2, height / 2 - space.height /2);
+  
 }
